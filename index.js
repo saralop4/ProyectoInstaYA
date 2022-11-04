3f669fdfadb597afa4ad52a5212d4fd7165681b8
Sarah Lopez <93948109+saralop4@users.noreply.github.com>
2022-11-03 22:01:06 -0500

    init commit

---
+++ index.js
@@ -0,0 +1,85 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://ROOT:LODfQLo6oHJAzhR3@cluster0.414dubm.mongodb.net/test";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+var collection = null;
+client.connect(err => {
+
+  collection = client.db("paquetesdb").collection("usuarios");
+
+})
+
+function crearUsuario (user){
+  collection.insertOne(user);
+}
+
+
+
+const express = require('express')
+
+const app = express()
+app.use(express.json());
+
+
+const port = 3000
+
+app.post('/crearUsuario', (req, res) => {
+
+  try {
+
+    let usuario = req.body.usuario
+    let contrasena = req.body.contrasena
+    let nombre = req.body.nombre
+    let correo = req.body.correo
+
+    invalidos = []
+
+    if(!usuario){
+      invalidos.push('usuario')
+    }
+
+    if(!contrasena){
+      invalidos.push('contrasena')
+    }
+
+    if(!nombre){
+      invalidos.push('nombre')
+    }
+    
+    if(!correo){
+      invalidos.push('correo')
+    }
+
+    if(invalidos.length == 0){
+     crearUsuario({
+      usuario,contrasena,nombre,correo
+     })
+      res.send({status:true,mesaje:"Todo Ok"})
+    }else{
+
+      console.log(invalidos)
+      mensaje = "Por favor enviar:"
+      invalidos.forEach(element => {
+        mensaje += element+', '
+      });
+      res.send({status:false,mesaje: mensaje})
+
+    }
+
+    
+  } catch (error) {
+    res.send({status:false,mesaje:error})
+  }
+ 
+})
+
+app.get('/editarUsuario', (req, res) => {
+  res.send('Editar')
+})
+
+app.get('/eliminarUsuario', (req, res) => {
+  res.send('Eliminar')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
