c1767da06fef97e65243de6f851a99bb162b7894
Sarah Lopez <93948109+saralop4@users.noreply.github.com>
2022-11-03 22:24:13 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,53 +6,53 @@ client.connect(err => {
 
   collection = client.db("paquetesdb").collection("usuarios");
 
+
+  const express = require('express')
+
+  const app = express()
+  app.use(express.json());
+  
+  
+  const port = process.env.PORT || 5000
+  
+  app.post('/crearUsuario', (req, res) => {
+  
+    try {
+  
+      let usuario = req.body.usuario
+      let contrasena = req.body.contrasena
+      let nombre = req.body.nombre
+      let correo = req.body.correo
+  
+      crearUsuario({
+        usuario,contrasena,nombre,correo
+       })
+        res.send({status:true,mesaje:"Todo Ok"})
+  
+      
+    } catch (error) {
+  
+      console.log(error)
+      res.send({status:false,mesaje:error})
+    }
+   
+  })
+  
+  app.get('/editarUsuario', (req, res) => {
+    res.send('Editar')
+  })
+  
+  app.get('/eliminarUsuario', (req, res) => {
+    res.send('Eliminar')
+  })
+  
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
+  
 })
 
 function crearUsuario (user){
   collection.insertOne(user);
 }
 
-
-
-const express = require('express')
-
-const app = express()
-app.use(express.json());
-
-
-const port = process.env.PORT || 5000
-
-app.post('/crearUsuario', (req, res) => {
-
-  try {
-
-    let usuario = req.body.usuario
-    let contrasena = req.body.contrasena
-    let nombre = req.body.nombre
-    let correo = req.body.correo
-
-    crearUsuario({
-      usuario,contrasena,nombre,correo
-     })
-      res.send({status:true,mesaje:"Todo Ok"})
-
-    
-  } catch (error) {
-
-    console.log(error)
-    res.send({status:false,mesaje:error})
-  }
- 
-})
-
-app.get('/editarUsuario', (req, res) => {
-  res.send('Editar')
-})
-
-app.get('/eliminarUsuario', (req, res) => {
-  res.send('Eliminar')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
