f9b7693cf4e65292b1cc8517ede4b29a61fc32b8
Sarah Lopez <93948109+saralop4@users.noreply.github.com>
2022-11-04 16:26:15 -0500

    Update index.js

--- index.js
+++ index.js
@@ -17,6 +17,20 @@ const app = express()
 app.use(express.json());
 
 
+const allowedOrigins = ['127.0.0.1'];
+app.use(cors({
+  origin: function(origin, callback){
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+
+}));
+
+
 const port = process.env.PORT || 5000
 
 app.post('/crearUsuario', (req, res) => {
