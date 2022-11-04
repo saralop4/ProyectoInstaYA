63692bdb93fa5900e0abf23d56292a61e0e958ca
Sarah Lopez <93948109+saralop4@users.noreply.github.com>
2022-11-03 22:31:06 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,58 +1,62 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://ROOT:LODfQLo6oHJAzhR3@cluster0.414dubm.mongodb.net/test";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-var collection = null;
-client.connect(err => {
-
-  collection = client.db("paquetesdb").collection("usuarios");
 
 
-  const express = require('express')
 
-  const app = express()
-  app.use(express.json());
-  
-  
-  const port = process.env.PORT || 5000
-  
-  app.post('/crearUsuario', (req, res) => {
-  
-    try {
-  
-      let usuario = req.body.usuario
-      let contrasena = req.body.contrasena
-      let nombre = req.body.nombre
-      let correo = req.body.correo
-  
-      crearUsuario({
-        usuario,contrasena,nombre,correo
-       })
-        res.send({status:true,mesaje:"Todo Ok"})
-  
-      
-    } catch (error) {
-  
-      console.log(error)
-      res.send({status:false,mesaje:error})
-    }
-   
-  })
-  
-  app.get('/editarUsuario', (req, res) => {
-    res.send('Editar')
-  })
-  
-  app.get('/eliminarUsuario', (req, res) => {
-    res.send('Eliminar')
-  })
-  
-  app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-  })
-  
+const express = require('express')
+
+const app = express()
+app.use(express.json());
+
+
+const port = process.env.PORT || 5000
+
+app.post('/crearUsuario', (req, res) => {
+
+  try {
+
+    let usuario = req.body.usuario
+    let contrasena = req.body.contrasena
+    let nombre = req.body.nombre
+    let correo = req.body.correo
+
+    crearUsuario({
+      usuario,contrasena,nombre,correo
+     })
+      res.send({status:true,mesaje:"Todo Ok"})
+
+    
+  } catch (error) {
+
+    console.log(error)
+    res.send({status:false,mesaje:error})
+  }
+ 
 })
 
-function crearUsuario (user){
-  collection.insertOne(user);
+app.get('/editarUsuario', (req, res) => {
+  res.send('Editar')
+})
+
+app.get('/eliminarUsuario', (req, res) => {
+  res.send('Eliminar')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+
+function crearUsuario (user) {
+  client.connect(err => {
+    var collection = null;
+    collection = client.db("paquetesdb").collection("usuarios");
+    collection.insertOne(user);
+    mongoClient.close();
+  
+  })
+  
+  
 }
 
