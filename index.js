e5e8c4804edc22164444c7f82371b0ed16f93e93
Sarah Lopez <93948109+saralop4@users.noreply.github.com>
2022-11-04 15:51:02 -0500

    feat: creado curd

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const ObjectID = require('mongodb').ObjectID;
 const uri = "mongodb+srv://ROOT:LODfQLo6oHJAzhR3@cluster0.414dubm.mongodb.net/test";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -22,14 +23,17 @@ app.post('/crearUsuario', (req, res) => {
 
   try {
 
+    let id = req.body.id
     let usuario = req.body.usuario
     let contrasena = req.body.contrasena
     let nombre = req.body.nombre
     let correo = req.body.correo
 
-    crearUsuario({
+
+
+    collection.insertOne({
       usuario,contrasena,nombre,correo
-     })
+    });
       res.send({status:true,mesaje:"Todo Ok"})
 
     
@@ -41,23 +45,74 @@ app.post('/crearUsuario', (req, res) => {
  
 })
 
-app.get('/editarUsuario', (req, res) => {
-  res.send('Editar')
+app.get('/EditarUsuario/:id', (req, res) => {
+
+try {
+  const userId = req.params.id;
+
+  let usuario = req.body.usuario
+  let contrasena = req.body.contrasena
+  let nombre = req.body.nombre
+  let correo = req.body.correo
+
+   collection.updateOne(
+    { _id: new ObjectID(userId) },
+    { $set:  { usuario,contrasena,nombre,correo}},
+    { upsert: true }
+  )
+
+  res.send({status:true,mesaje:"Todo Ok"})
+} catch (error) {
+  console.log(error)
+  res.send({status:false,mesaje:error})
+}
+
+
+
 })
 
-app.get('/eliminarUsuario', (req, res) => {
-  res.send('Eliminar')
+app.get('/VerUsuario/:id', (req, res) => {
+
+ try {
+  
+  const userId = req.params.id;
+
+  var data = collection.findOne(
+    { _id: new ObjectID(userId) },
+    (err,doc)=>{
+      res.send({status:true,mesaje:"Todo Ok",data:doc})
+    }
+  )
+
+
+ } catch (error) {
+  console.log(error)
+  res.send({status:false,mesaje:error})
+ }
+
+})
+
+app.delete('/eliminarUsuario/:id', (req, res) => {
+
+  try {
+    
+    const userId = req.params.id;
+  
+    collection.deleteOne(
+      { _id: new ObjectID(userId) }
+    )
+
+    res.send({status:true,mesaje:"Todo Ok"})
+
+  } catch (error) {
+    console.log(error)
+  res.send({status:false,mesaje:error})
+  }
+
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
-
-function crearUsuario (user){
- 
-  collection.insertOne(user);
-  
-  
-}
 
