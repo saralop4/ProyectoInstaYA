ad6f6e52fe052f76374e5518cea206a9a2b99c13
Sarah Lopez <93948109+saralop4@users.noreply.github.com>
2022-11-04 16:29:07 -0500

    feat: se implementaron cors

--- index.js
+++ index.js
@@ -12,23 +12,29 @@ client.connect(err => {
 })
 
 const express = require('express')
+const cors = require('cors')
 
 const app = express()
 app.use(express.json());
 
 
 const allowedOrigins = ['127.0.0.1'];
-app.use(cors({
-  origin: function(origin, callback){
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){
-      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  }
 
-}));
+app.use(
+
+  cors({
+    origin: function(origin, callback){
+      if(!origin) return callback(null, true);
+      if(allowedOrigins.indexOf(origin) === -1){
+        const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    }
+
+})
+
+);
 
 
 const port = process.env.PORT || 5000
